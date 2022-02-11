7b50c8ae055af118da4583d0fa13c55b32b8226f
EnnaqarBoubker <ennaqar.boubker5@gmail.com>
2022-02-11 15:52:52 +0100

    connected database with page student and creat the requste select

--- student.php
+++ student.php
@@ -12,12 +12,14 @@
     // );
 
         
+        
     $image = <<<IMAGE
         <img class="w-50 rounded-1" src="./image/youcode.png" alt="user">
         IMAGE;
 
-
-
+                //connect db in php page
+                include './breif-php/conndb.php';
+                
                 include './breif-php/head.php';
                 include './breif-php/sidebar.php'; 
                 include './breif-php/header.php';
@@ -35,12 +37,14 @@
                                     <i class="fa fa-sort-asc " aria-hidden="true"></i>
                                     <i class="fa fa-sort-desc" aria-hidden="true"></i>
                                 </div>
-                                <a href="./add-stud.php" class="btn d-none d-lg-inline btn-sm btn-primary btn-create p-2">ADD NEW STUDENT</a>
-                                <a href="./add-stud.php" class="btn d-inline d-lg-none d-md-none btn-sm btn-primary btn-create p-2">
+                                <a class="btn d-none d-lg-inline btn-sm btn-primary btn-create p-2" data-bs-toggle="modal" data-bs-target="#add">ADD NEW STUDENT</a>
+                                <a class="btn d-inline d-lg-none d-md-none btn-sm btn-primary btn-create p-2" data-bs-toggle="modal" data-bs-target="#add">
                                     <i class="fa fa-user-plus" aria-hidden="true"></i>
                                 </a>
                             </div>
                         </div>
+
+                    
                         <div class="height table-responsive">
                             <table class="table table-borderless">
                                 <thead class=" text-color fs-6 fw-normal">
@@ -58,27 +62,41 @@
 
                                     <?php 
 
-                                        $students = file_get_contents("student.json");
-                                        $students = json_decode($students, true);
+                                        // $students = file_get_contents("student.json");
+                                        // $students = json_decode($students, true);
 
-                                        foreach ($students as $key => $val) :
-                                           
+
+                                        // READ DB with CRUD
+                                        
+                                        $sql = "SELECT * FROM student_a"; //var dans un requte
+
+                                        $result = mysqli_query($conn, $sql);
+
+                                        if($result) {
+                                            while($row = mysqli_fetch_array($result)){ 
+                                                $name            = $row['name'];
+                                                $mail            = $row['e_mail'];
+                                                $phone           = $row['phone'];
+                                                $enroll_Number   = $row['enroll_number'];
+                                                $DateOfAdmission = $row['DateOfAdmission'];
                                     ?> 
 
                                 <tr class="bg-withe">
                                     <td class="align-middle"><?php echo $image ?></td>
-                                    <td class="align-middle"><?php echo $val['Name']; ?></td>
-                                    <td class="align-middle"><?php echo $val['E-mail']; ?></td>
-                                    <td class="align-middle"><?php echo $val['Phone']; ?></td>
-                                    <td class="align-middle"><?php echo $val['Number']; ?></td>
-                                    <td class="align-middle"><?php echo $val['Admission']; ?></td>
+                                    <td class="align-middle"><?php echo $row['name']; ?></td>
+                                    <td class="align-middle"><?php echo $row['e_mail']; ?></td>
+                                    <td class="align-middle"><?php echo $row['phone']; ?></td>
+                                    <td class="align-middle"><?php echo $row['enroll_number']; ?></td>
+                                    <td class="align-middle"><?php echo $row['DateOfAdmission']; ?></td>
                                     <td class="align-middle">
-                                        <a href="edit-stud.php?index=<?php echo $key; ?>" class="fs-5 add text-decoration-none me-2" title="Add"> <i class="fa fa-pencil" aria-hidden="true"></i></a>
-                                        <a href="delet-stud.php?index=<?php echo $key; ?>" class="fs-5 delete text-decoration-none" title="Delete"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                        <a href="edit-stud.php?id=<?php echo $row['id'];?>" class="fs-5 add text-decoration-none me-2" title="Edite"> <i class="fa fa-pencil" aria-hidden="true"></i></a>
+                                        <a href="delet-stud.php?id=<?php echo $row['id']; ?>" class="fs-5 delete text-decoration-none" title="Delete"><i class="fa fa-trash" aria-hidden="true"></i></a>
                                     </td>
                                 </tr> 
                                 
-                                <?php  endforeach;?>
+                                <?php  }
+                                }
+                                ?>
 
                                 </tbody>
                             </table>
@@ -89,6 +107,7 @@
         </div>
     </div>
 
+    
     <?php
         include './breif-php/footre.php';
    ?>
