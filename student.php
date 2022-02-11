5336fa4cf8ae7d9562c94d1909e4559c4cdd376b
EnnaqarBoubker <ennaqar.boubker5@gmail.com>
2022-02-11 15:56:18 +0100

    added the title for popup

--- student.php
+++ student.php
@@ -44,7 +44,73 @@
                             </div>
                         </div>
 
+                        <!-- creat add student popup in page student -->
+                        <?php
+                                if (isset($_POST['save'])) {
+
+                                    $name            = $_POST['name'];
+                                    $mail            = $_POST['e_mail'];
+                                    $phone           = $_POST['phone'];
+                                    $enroll_n        = $_POST['enroll_number'];
+                                    $admission       = $_POST['DateOfAdmission'];
                     
+                                    $sql = "INSERT INTO student_a (name, e_mail, phone, enroll_number, DateOfAdmission ) VALUES ('$name', '$mail', '$phone', '$enroll_n', '$admission')";
+                    
+                                    $result = mysqli_query($conn, $sql);
+                    
+                    
+                                    if (!$result) {
+                                        die(mysqli_error($conn)); 
+                                    }
+
+                                } 
+                        ?>
+
+                        <div class="modal fade" id="add" tabindex="-1" aria-labelledby="added" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                     <div class="modal-header">
+                                         <form method="POST" class="form-group w-100">
+                                                    
+                                                <div class="form-outline">
+                                                    <label class="form-label">User name</label>
+                                                    <input type="text" class="form-control form-group" name="name"/>
+                                                    
+                                                </div>
+
+                                                <div class="form-outline">
+                                                    <label class="form-label">E-mail</label>
+                                                    <input type="email" class="form-control form-group" name="e_mail"/>
+                                                    
+                                                </div>
+
+                                                <div class="form-outline">
+                                                    <label class="form-label">Phone</label>
+                                                    <input type="text" class="form-control form-group" name="phone"/>
+                                                    
+                                                </div>
+
+                                                <div class="form-outline">
+                                                    <label class="form-label">Enroll Number</label>
+                                                    <input type="text" class="form-control form-group" name="enroll_number"/>
+                                                    
+                                                </div>
+
+                                                <div class="form-outline mb-3">
+                                                    <label class="form-label">Date of admission</label>
+                                                    <input type="date" class="form-control form-group" name="DateOfAdmission"/>
+                                                    
+                                                </div>
+
+                                                <button type="submit" class="btn btn-primary btn-block" name="save">Envoyer</button>
+                                        </form>
+                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end creat add student popup in page student -->
+
+
                         <div class="height table-responsive">
                             <table class="table table-borderless">
                                 <thead class=" text-color fs-6 fw-normal">
