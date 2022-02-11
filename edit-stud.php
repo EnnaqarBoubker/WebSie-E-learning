62c1578ce81e1bbf8b68c64ba1566c42265debf4
EnnaqarBoubker <ennaqar.boubker5@gmail.com>
2022-02-11 16:00:36 +0100

    switch file json with database SQL in page edit student

--- edit-stud.php
+++ edit-stud.php
@@ -1,11 +1,38 @@
 <?php 
-    $index= $_GET['index'];
+   include './breif-php/conndb.php';
 
-    $students = file_get_contents('student.json');
-    $data = json_decode($students ,true);
+   $id = $_GET['id'];
 
-    $student = $data[$index];
+   $sql = "SELECT * FROM student_a WHERE id = $id";
 
+   $resulet = mysqli_query($conn, $sql);
+
+   $row = mysqli_fetch_array($resulet);
+        $name            = $row['name'];
+        $mail            = $row['e_mail'];
+        $phone           = $row['phone'];
+        $enroll_n        = $row['enroll_number'];
+        $admission       = $row['DateOfAdmission'];
+
+   if(isset($_POST['save'])){
+        $name            = $_POST['name'];
+        $mail            = $_POST['e_mail'];
+        $phone           = $_POST['phone'];
+        $enroll_n        = $_POST['enroll_number'];
+        $admission       = $_POST['DateOfAdmission'];
+
+        $sql = "UPDATE student_a SET name = '$name', e_mail = '$mail', phone = '$phone', enroll_number = '$enroll_n', DateOfAdmission = '$admission' WHERE id = '$id'"; 
+   
+        $resulet = mysqli_query($conn, $sql);
+        if($resulet){
+            header("location: student.php");
+        }
+        else{
+            die(mysqli_error($conn));
+        }
+
+   
+    }
 
     include './breif-php/head.php';
 
@@ -14,65 +41,38 @@
     <div class="container">
         <form method="POST">
            <a href="./student.php" class="my-4 btn btn-primary btn-create p-2">BACk</a>
-            <!-- 2 column grid layout with text inputs for the first and last names -->
+            
                 
             <div class="form-outline">
-                <label class="form-label" for="form6Example2">User name</label>
-                <input type="text" id="form6Example2" class="form-control" name="Name" value="<?php echo $student['Name']; ?>"/>
+                <label class="form-label">User name</label>
+                <input type="text" class="form-control" name="name" value="<?php echo $row['name']; ?>"/>
                 
             </div>
 
-            <!-- Text input -->
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form6Example3">E-mail</label>
-                <input type="email" id="form6Example3" class="form-control" name="E-mail" value="<?php echo $student['E-mail']; ?>"/>
+          
+            <div class="form-outline mb-3">
+                <label class="form-label">E-mail</label>
+                <input type="email" class="form-control" name="e_mail" value="<?php echo $row['e_mail']; ?>"/>
                 
             </div>
-
-            <!-- Text input -->
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form6Example4">Phone</label>
-                <input type="text" id="form6Example4" class="form-control" name="Phone" value="<?php echo $student['Phone']; ?>"/>
+            <div class="form-outline mb-3">
+                <label class="form-label">Phone</label>
+                <input type="text" class="form-control" name="phone" value="<?php echo $row['phone']; ?>"/>
                 
             </div>
-
-            <!-- Message input -->
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form6Example7">Enroll Number</label>
-                <input type="text" id="form6Example7" class="form-control" name="Number" value="<?php echo $student['Number']; ?>"/>
+            <div class="form-outline mb-3">
+                <label class="form-label">Enroll Number</label>
+                <input type="text" class="form-control" name="enroll_number" value="<?php echo $row['enroll_number']; ?>"/>
                 
             </div>
-
-            <div class="form-outline mb-4">
-                <label class="form-label" for="form6Example8">Date of admission</label>
-                <input type="date" id="form6Example8" class="form-control" name="Admission" value="<?php echo $student['Admission']; ?>"/>
+            <div class="form-outline mb-3">
+                <label class="form-label">Date of admission</label>
+                <input type="date" class="form-control" name="DateOfAdmission" value="<?php echo $row['DateOfAdmission']; ?>"/>
                 
             </div>
-            <!-- Submit button -->
-            <button type="submit" class="btn btn-primary btn-block mb-4" name="save">Envoyer</button>
+    
+            <button type="submit" class="btn btn-primary btn-block mb-4" name="save">SAVE</button>
         </form>
     </div>
 
-    <?php
-        if(isset($_POST['save'])){
-
-         
-        $input = array(
-            'Name'  => $_POST['Name'],
-            'E-mail'  => $_POST['E-mail'],
-            'Phone'  => $_POST['Phone'],
-            'Number'  => $_POST['Number'],
-            'Admission'  => $_POST['Admission']
-
-        );
-
-        $data[$index] = $input;
-
-
-        $students = json_encode($data, JSON_PRETTY_PRINT);
-		file_put_contents('student.json', $students);
-
-
-        header('location: student.php');
-        }
-    ?>
+<?php include './breif-php/footre.php';?>
